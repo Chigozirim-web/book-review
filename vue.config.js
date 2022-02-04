acfd4655fdda7c8fa8cb1a499fe249654b7befc9
Chigozirim-web <arukwemargaret@gmail.com>
2022-02-05 00:57:06 +0100

    dist subtree

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-
-if (process.env.NODE_ENV === 'development'){
-    process.env.VUE_APP_API_URL ='http://localhost:5000'
-}
-
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/book-review/' : '/',
- 
-}
